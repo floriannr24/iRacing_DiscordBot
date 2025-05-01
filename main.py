22d777cf470bc44254cd3650a467bf57450a529e
Florian Niedermeier <florian-niedermeier@gmx.de>
2025-05-01 02:16:02 +0200

    print environ items

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from _bot.bot import DiscordBot
 import os
+
 from dotenv import load_dotenv
+
+from _bot.bot import DiscordBot
 
 if __name__ == "__main__":
 
@@ -9,7 +11,12 @@ if __name__ == "__main__":
     load_dotenv(f".env.{environment}")
 
     if environment == "production":
+
+        for key, value in os.environ.items():
+            print(f"{key}={value}")
+
         bot = DiscordBot()
         bot.run()
+
     else:
         raise Exception("No production run config found")
